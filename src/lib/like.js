5b4f594bcfe523c816d4b468b35c51a8704b861a
Douglas Lessa <douglaslessa@wizsolucoes.com.br>
2020-11-19 11:06:07 -0300

    feat: adicionar autenticação e injeção de token

--- src/lib/like.js
+++ src/lib/like.js
@@ -1,73 +1,161 @@
 export class Like {
 
-    constructor(endpoint){
+    constructor(endpoint, trackCode){
         this.endpoint = endpoint;
+        this.trackCode = trackCode;
     }
 
-    like(postId, postTitle, userId, userName, createdDate){
+    auth(){
         return new Promise((resolve, reject) => {
+            const authLike = window.localStorage.getItem("auth_like"); 
+            if(authLike){
+                resolve(JSON.parse(authLike));
+            }
             request(
-                (createdDate ? 'PUT' : 'POST'),
-                (createdDate ? `${this.endpoint}/api/v1/like/user/${userId}/post/${postId}/liked` : `${this.endpoint}/api/v1/like`),
-                (createdDate ? {
-                        'liked': true
-                    } : {
-                        'id': postId,
-                        'user': {
-                            'id': userId,
-                            'name': userName
-                        },
-                        'title': postTitle,
-                        'liked': true
-                    }
-                )
+                'post',
+                `${this.endpoint}/api/v1/auth`,
+                {
+                    'trackCode': this.trackCode
+                }
             ).then((data) => {
+                window.localStorage.setItem("auth_like", JSON.stringify(data));
                 resolve(data);
             }, (error) => {
                 reject(error);
             });
+        });
+    }
+
+    get(postId, userId){
+        return new Promise((resolve, reject) => {
+            auth()
+                .then(
+                    response => {
+                        resolve(
+                            new Promise((resolve, reject) => {
+                                request(
+                                    'GET',
+                                    `${this.endpoint}/api/v1/like/user/${userId}/post/${postId}`,
+                                    response.accessToken
+                                ).then((data) => {
+                                    resolve(data);
+                                }, (error) => {
+                                    reject(error);
+                                });
+                            })
+                        );
+                    },
+                    error => {
+                        reject(error);
+                    }
+                )
+        });
+    }
+
+    like(postId, postTitle, userId, userName, createdDate){
+        return new Promise((resolve, reject) => {
+            auth()
+                .then(
+                    response => {
+                        resolve(
+                            new Promise((resolve, reject) => {
+                                request(
+                                    (createdDate ? 'PUT' : 'POST'),
+                                    (createdDate ? `${this.endpoint}/api/v1/like/user/${userId}/post/${postId}/liked` : `${this.endpoint}/api/v1/like`),
+                                    response.accessToken,
+                                    (createdDate ? {
+                                            'liked': true
+                                        } : {
+                                            'id': postId,
+                                            'user': {
+                                                'id': userId,
+                                                'name': userName
+                                            },
+                                            'title': postTitle,
+                                            'liked': true
+                                        }
+                                    )
+                                ).then((data) => {
+                                    resolve(data);
+                                }, (error) => {
+                                    reject(error);
+                                });
+                            })
+                        );
+                    },
+                    error => {
+                        reject(error);
+                    }
+                )
         });
     }
 
     dislike(postId, postTitle, userId, userName, createdDate){
         return new Promise((resolve, reject) => {
-            request(
-                (createdDate ? 'PUT' : 'POST'),
-                (createdDate ? `${this.endpoint}/api/v1/like/user/${userId}/post/${postId}/liked` : `${this.endpoint}/api/v1/like`),
-                (createdDate ? {
-                        'liked': false
-                    } : {
-                        'id': postId,
-                        'user': {
-                            'id': userId,
-                            'name': userName
-                        },
-                        'title': postTitle,
-                        'liked': false
+            auth()
+                .then(
+                    response => {
+                        resolve(
+                            new Promise((resolve, reject) => {
+                                request(
+                                    (createdDate ? 'PUT' : 'POST'),
+                                    (createdDate ? `${this.endpoint}/api/v1/like/user/${userId}/post/${postId}/liked` : `${this.endpoint}/api/v1/like`),
+                                    response.accessToken,
+                                    (createdDate ? {
+                                            'liked': false
+                                        } : {
+                                            'id': postId,
+                                            'user': {
+                                                'id': userId,
+                                                'name': userName
+                                            },
+                                            'title': postTitle,
+                                            'liked': false
+                                        }
+                                    )
+                                ).then((data) => {
+                                    resolve(data);
+                                }, (error) => {
+                                    reject(error);
+                                });
+                            })
+                        );
+                    },
+                    error => {
+                        reject(error);
                     }
                 )
-            ).then((data) => {
-                resolve(data);
-            }, (error) => {
-                reject(error);
-            });
         });
     }
 
     undo(postId, userId){
         return new Promise((resolve, reject) => {
-            request(
-                'DELETE',
-                `${this.endpoint}/api/v1/like/user/${userId}/post/${postId}`
-            ).then((data) => {
-                resolve(data);
-            }, (error) => {
-                reject(error);
-            });
+            auth()
+                .then(
+                    response => {
+                        resolve(
+                            new Promise((resolve, reject) => {
+                                request(
+                                    'DELETE',
+                                    `${this.endpoint}/api/v1/like/user/${userId}/post/${postId}`,
+                                    response.accessToken
+                                ).then((data) => {
+                                    resolve(data);
+                                }, (error) => {
+                                    reject(error);
+                                });
+                            })
+                        );
+                    },
+                    error => {
+                        reject(error);
+                    }
+                )
         });
     }
     
-    request(method, url, data) {
+    request(method, url, accessToken, data) {
+        auth();
         return new Promise((resolve, reject) => {
             var xmlHttp = new XMLHttpRequest();
             xmlHttp.open(method, url, true);
@@ -81,6 +169,7 @@ export class Like {
                     }
                 }
             }
+            xmlHttp.setRequestHeader('Authorization', `Bearer ${accessToken}`);
             if(data){
                 xmlHttp.setRequestHeader('Content-Type', 'application/json');
                 xmlHttp.send(JSON.stringify(data));
